09eec4ebf4f626084426cba35e10cbb43016de6b
Mark Otterson <otterson.2@gmail.com>
2021-11-26 22:33:03 -0500

    add parameterized route

--- app/controllers/PlayersController.java
+++ app/controllers/PlayersController.java
@@ -6,4 +6,7 @@ public class PlayersController extends Controller {
     public Result index() {
         return ok(views.html.players.render());
     }
+    public Result showPlayer(String id) {
+        return ok(views.html.player.render(id));
+    }
 }
